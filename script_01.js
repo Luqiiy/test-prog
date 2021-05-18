4b2d65ae303f4bd812d5ee1850ef7aacf05fd98c
Luqiiy <a.schlotte97@gmail.com>
2021-05-18 11:35:10 +0200

    Deklaration + Wertzuweisung 1

--- script_01.js
+++ script_01.js
@@ -1,1 +1,22 @@
 /***** Variablen 01 *******/
+
+// ich gebe da iwas ein
+
+/**
+  block kommentar
+ */
+
+// Ausgabe
+//console.log("Hallo Welt");
+//console.log(firstName);
+
+// Deklaration + Wertzuweisung 1
+
+let firstName; // Deklaration (Definition)
+firstName = "Anthony"; // Wertzuweisung
+console.log(firstName); // Ausgabe
+let familyName = "Schlotte"; // Deklaration + Wertzuweisung
+console.log(familyName); // Ausgabe
+console.log(firstName + " " + familyName);
+
+
