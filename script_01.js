0b7e5b4d57263162d1112f6b0c8feeaba80b37b4
Luqiiy <a.schlotte97@gmail.com>
2021-05-18 12:30:54 +0200

    JS ist eine untypisierte Sprache! | untyped

--- script_01.js
+++ script_01.js
@@ -21,7 +21,20 @@ console.log(firstName + " " + familyName);
 */
 
 // Deklaration + Wertzuweisung 2
+/*
 let firstName,familyName;
 firstName = prompt("Bitte Vornamen eingeben:");
 familyName = prompt("Bitte Namen eingeben:");
 console.log("moin, " + firstName + " " + familyName + "!");
+*/
+
+// JS ist eine untypisierte Sprache! | untyped
+
+let test; // Deklaration
+test = "hi"; //string
+test = 2; // number 
+test = true; //boolean
+
+console.log("Typ: " + typeof test);
+console.log("Inhalt: " + test);
+
