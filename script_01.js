85f635999e54798ef26d7a2546758ae7d1d5fd87
Luqiiy <a.schlotte97@gmail.com>
2021-05-18 12:03:32 +0200

    Deklaration + Wertzuweisung 2

--- script_01.js
+++ script_01.js
@@ -11,12 +11,17 @@
 //console.log(firstName);
 
 // Deklaration + Wertzuweisung 1
-
+/*
 let firstName; // Deklaration (Definition)
 firstName = "Anthony"; // Wertzuweisung
 console.log(firstName); // Ausgabe
 let familyName = "Schlotte"; // Deklaration + Wertzuweisung
 console.log(familyName); // Ausgabe
 console.log(firstName + " " + familyName);
+*/
 
-
+// Deklaration + Wertzuweisung 2
+let firstName,familyName;
+firstName = prompt("Bitte Vornamen eingeben:");
+familyName = prompt("Bitte Namen eingeben:");
+console.log("moin, " + firstName + " " + familyName + "!");
